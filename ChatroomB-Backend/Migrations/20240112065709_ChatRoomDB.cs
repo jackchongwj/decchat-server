cad01476a99ad190195abca3a0fe94ba43bfd158
leow <leowjm69@gmail.com>
2024-01-12 14:58:47 +0800

    - add models - add migrations - add connection string - change  Entity Framework library to 8.0 version

---
+++ ChatroomB-Backend/Migrations/20240112065709_ChatRoomDB.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatroomB_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChatRoomDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatRooms",
+                columns: table => new
+                {
+                    ChatRoomId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RoomType = table.Column<bool>(type: "bit", nullable: false),
+                    RoomProfilePic = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InitiatedBy = table.Column<int>(type: "int", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatRooms", x => x.ChatRoomId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "varchar(15)", nullable: false),
+                    ProfileName = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(256)", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "varchar(256)", nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Friends",
+                columns: table => new
+                {
+                    RequestId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderId = table.Column<int>(type: "int", nullable: true),
+                    ReceiverId = table.Column<int>(type: "int", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Friends", x => x.RequestId);
+                    table.ForeignKey(
+                        name: "FK_Friends_Users_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                    table.ForeignKey(
+                        name: "FK_Friends_Users_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RefreshToken",
+                columns: table => new
+                {
+                    TokenId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    TokenHash = table.Column<string>(type: "varchar(256)", nullable: false),
+                    ExpiredDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshToken", x => x.TokenId);
+                    table.ForeignKey(
+                        name: "FK_RefreshToken_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserChatRooms",
+                columns: table => new
+                {
+                    UserChatRoomId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    ChatRoomId = table.Column<int>(type: "int", nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserChatRooms", x => x.UserChatRoomId);
+                    table.ForeignKey(
+                        name: "FK_UserChatRooms_ChatRooms_ChatRoomId",
+                        column: x => x.ChatRoomId,
+                        principalTable: "ChatRooms",
+                        principalColumn: "ChatRoomId");
+                    table.ForeignKey(
+                        name: "FK_UserChatRooms_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessengeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    UserChatRoomId = table.Column<int>(type: "int", nullable: true),
+                    TimeStamp = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ResourceUrl = table.Column<string>(type: "varchar(256)", nullable: true),
+                    MessageType = table.Column<int>(type: "int", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessengeId);
+                    table.ForeignKey(
+                        name: "FK_Messages_UserChatRooms_UserChatRoomId",
+                        column: x => x.UserChatRoomId,
+                        principalTable: "UserChatRooms",
+                        principalColumn: "UserChatRoomId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friends_ReceiverId",
+                table: "Friends",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friends_SenderId",
+                table: "Friends",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserChatRoomId",
+                table: "Messages",
+                column: "UserChatRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshToken_UserId",
+                table: "RefreshToken",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserChatRooms_ChatRoomId",
+                table: "UserChatRooms",
+                column: "ChatRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserChatRooms_UserId",
+                table: "UserChatRooms",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Friends");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "RefreshToken");
+
+            migrationBuilder.DropTable(
+                name: "UserChatRooms");
+
+            migrationBuilder.DropTable(
+                name: "ChatRooms");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- ChatroomB-Backend/Models/Messages.cs
+++ ChatroomB-Backend/Models/Messages.cs
@@ -6,22 +6,24 @@ namespace ChatroomB_Backend.Models
     public class Messages
     {
         [Key]
-        public int messengeId { get; set; }
+        public int? MessengeId { get; set; }
 
         [Required]
-        public string content { get; set; } = null!;
+        [Column(TypeName = "nvarchar(200)")]
+        public string Content { get; set; } = null!;
 
         public UserChatRooms? UserChatRooms { get; set; }
         [ForeignKey("UserChatRooms")]
-        public int userChatRoomId { get; set; }
+        public int? UserChatRoomId { get; set; }
 
-        public DateTime timeStamp { get; set; }
+        public DateTime? TimeStamp { get; set; }
 
-        public string? resourceUrl { get; set; }
+        [Column(TypeName = "varchar(256)")]
+        public string? ResourceUrl { get; set; }
 
         [Required]
-        public int messageType { get; set; }
+        public int MessageType { get; set; }
 
-        public bool isDelete { get; set; }
+        public bool IsDelete { get; set; }
     }
 }
