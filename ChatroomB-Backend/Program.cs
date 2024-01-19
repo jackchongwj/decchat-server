9c8b39037cb0f780e70dae5e59c3994c9186becd
leow <leowjm69@gmail.com>
2024-01-19 15:49:11 +0800

    - uncomment db contect in program

--- ChatroomB-Backend/Program.cs
+++ ChatroomB-Backend/Program.cs
@@ -9,8 +9,8 @@ using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDbContext<ChatroomB_BackendContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("ChatroomB_BackendContext") ?? throw new InvalidOperationException("Connection string 'ChatroomB_BackendContext' not found.")));
+builder.Services.AddDbContext<ChatroomB_BackendContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ChatroomB_BackendContext") ?? throw new InvalidOperationException("Connection string 'ChatroomB_BackendContext' not found.")));
 
 builder.Services.AddTransient<IDbConnection>((sp) =>
             new SqlConnection(builder.Configuration.GetConnectionString("ChatroomB_BackendContext")));
