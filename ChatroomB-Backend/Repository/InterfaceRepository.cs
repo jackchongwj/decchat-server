81cb96df07adf416bf2b695091fa6607a6804f25
Xiao Di <wongshenyangobest@gmail.com>
2024-01-22 10:00:22 +0800

    userrepo inter from w1

--- ChatroomB-Backend/Repository/InterfaceRepository.cs
+++ ChatroomB-Backend/Repository/InterfaceRepository.cs
@@ -9,6 +9,10 @@ namespace ChatroomB_Backend.Repository
     {
         Task<IEnumerable<Users>> GetByName(string profileName, int userId);                                                    //Get user by user profile name and filter friend request
         Task<IEnumerable<Users>> GetFriendRequest(int userId);                                                                 // Get All Friend request
+        Task<Users> GetUserById(int userId);
+        Task<int> UpdateUserProfile(Users userProfile);
+        Task<int> DeleteUserProfile(int userId);
+        Task<int> ChangePassword(int userId, string newPassword);
     }   
 
     public interface IFriendRepo
