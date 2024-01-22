d3b81605eeb7d17f5a3ebb9424e86329ab92b624
Xiao Di <wongshenyangobest@gmail.com>
2024-01-22 10:10:36 +0800

    user service inter from W1

--- ChatroomB-Backend/Service/InterfaceServices.cs
+++ ChatroomB-Backend/Service/InterfaceServices.cs
@@ -8,6 +8,10 @@ namespace ChatroomB_Backend.Service
     {
         Task<IEnumerable<Users>> GetByName(string profileName, int userId);                                                   //Get user by user profile name and filter friend request
         Task<IEnumerable<Users>> GetFriendRequest(int userId);                                                               //Get All Friend request
+        Task<Users> GetUserById(int userId);
+        Task<int> UpdateUser(Users user);
+        Task<int> DeleteUser(int userId);
+        Task<int> ChangePassword(int userId, string newPassword);
     }
 
     public interface IFriendService 
