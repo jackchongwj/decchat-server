6d2410a774108f4b98217fb1a318d115009e96b5
Jack 傑克 108 <dm108Jack@obestsolutions@onmicrosoft.com>
2024-03-03 23:43:47 +0800

    - add/improve renew access token logic

--- ChatroomB-Backend/Repository/TokenRepo.cs
+++ ChatroomB-Backend/Repository/TokenRepo.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System.Data;
 
 namespace ChatroomB_Backend.Repository
@@ -12,16 +13,23 @@ namespace ChatroomB_Backend.Repository
 
     {
         private readonly IDbConnection _dbConnection;
+        private readonly IConfiguration _config;
 
-        public TokenRepo(IDbConnection db) 
+        public TokenRepo(IDbConnection db, IConfiguration config)
         {
             _dbConnection = db;
+            _config = config;
         }
 
-        public async Task<bool> IsRefreshTokenValid(RefreshToken token)
+        public async Task<bool> IsRefreshTokenValid(RefreshToken token, int userId, string username)
         {
-            string sql = "exec CheckRefreshTokenValidity @Token";
-            int result = await _dbConnection.ExecuteScalarAsync<int>(sql, new { Token = token.Token });
+            string sql = "exec CheckRefreshTokenValidity @Token, @UserId, @UserName";
+            int result = await _dbConnection.ExecuteScalarAsync<int>(sql, new
+            {
+                Token = token.Token,
+                UserId = userId,
+                UserName = username
+            });
             bool isValid = Convert.ToBoolean(result);
 
             return isValid;
@@ -30,12 +38,14 @@ namespace ChatroomB_Backend.Repository
         public async Task<IActionResult> StoreRefreshToken(RefreshToken token)
         {
             string sql = "exec StoreRefreshToken @UserId, @Token, @ExpiredDateTime";
+            int expirationDays = _config.GetValue<int>("RefreshTokenSettings:ExpirationDays");
+            DateTime expirationDateTime = DateTime.UtcNow.AddDays(expirationDays);
 
             await _dbConnection.ExecuteAsync(sql, new RefreshToken
             {
                 UserId = token.UserId,
                 Token = token.Token,
-                ExpiredDateTime = token.ExpiredDateTime,
+                ExpiredDateTime = expirationDateTime,
             });
 
             return new OkObjectResult(new { Message = "Refresh token stored successfully" });
@@ -47,6 +57,21 @@ namespace ChatroomB_Backend.Repository
             await _dbConnection.ExecuteAsync(sql, new { token.Token });
 
             return new OkObjectResult(new { Message = "Refresh token removed successfully" });
+        }
+
+        public async Task<IActionResult> UpdateRefreshToken(RefreshToken token)
+        {
+            string sql = "exec UpdateRefreshTokenExpiry @Token, @ExpiredDateTime";
+            int expirationDays = _config.GetValue<int>("RefreshTokenSettings:ExpirationDays");
+            DateTime expirationDateTime = DateTime.UtcNow.AddDays(expirationDays);
+
+            await _dbConnection.ExecuteAsync(sql, new RefreshToken
+            {
+                Token = token.Token,
+                ExpiredDateTime = expirationDateTime,
+            });
+
+            return new OkObjectResult(token);
         }
     }
 }
